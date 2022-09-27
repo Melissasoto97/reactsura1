5cb78a7fc094ce1b16464bb02ff69b1209899121
407 <407@academicos.cesde.edu.co>
2022-09-27 17:40:10 -0500

    Se crea app react

---
+++ src/Componente/Melissa.js
@@ -0,0 +1,12 @@
+export function Melissa(){
+
+    let nombre="Melissa"
+    //Interpolar= Controlar la GUI desde js
+
+    return(
+        <>
+            <h1>HOLA REACT SOY {nombre} </h1>
+            <hr></hr>
+        </>
+    )
+}
